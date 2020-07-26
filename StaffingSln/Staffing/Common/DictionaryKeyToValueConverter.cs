04d362e0e118931f2627701b9c0e3498694313d9
Хасп Элд <EldHasp@Gmail.com>
2020-07-26 15:38:31 +0300

    Заменена реализация переключателя Представлений в Окне на конвертер-словарь.

--- StaffingSln/Staffing/Common/DictionaryKeyToValueConverter.cs
+++ StaffingSln/Staffing/Common/DictionaryKeyToValueConverter.cs
@@ -6,20 +6,21 @@ using System.Windows.Data;
 
 namespace Common
 {
-	/// <summary>Конвертер преобразующий ключ в значение по словарю</summary>
-	public class DictionaryKeyToValueConverter : Dictionary<object, object>, /* OnPropertyChangedClass,*/ IMultiValueConverter, IValueConverter
+    /// <summary>Конвертер преобразующий ключ в значение по словарю.</summary>
+    public class DictionaryKeyToValueConverter : Dictionary<object, object>,  IMultiValueConverter, IValueConverter
 	{
+		// Первым элементом в параметром values приходит ключ, вторым - словарь.
 		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
 		{
+			// Если пришёл только ключ, то используется словарь конвертера.
 			if (values.Length == 1)
 			{
-				//if (Dictionary?.Contains(values[0]) == true)
-				//    return Dictionary[values[0]];
 
 				if (TryGetValue(values[0], out object val))
 					return val;
 
 			}
+			// Если пришло больше одного элемента, то из первого извлекается ключ, из второго - словарь.
 			else if (values.Length > 1 && (values[1] is IDictionary dictionary) && (dictionary.Contains(values[0]) == true))
 			{
 				return dictionary[values[0]];
@@ -27,18 +28,9 @@ namespace Common
 			return null;
 		}
 
-
-		//public IDictionary Dictionary { get; private set; }
-		//public void SetDictionary(IDictionary dictionary)
-		//{
-		//    Dictionary = dictionary;
-		//    OnPropertyChanged(nameof(Dictionary));
-		//}
-
+		// В параметре value должен прийти клю словаря.
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			//if (value != null && Dictionary?.Contains(value) == true)
-			//    return Dictionary[value];
 			if (TryGetValue(value, out object val))
 				return val;
 
